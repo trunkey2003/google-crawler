245c6311cb14184c0f3622cc08402d4b0cdda97d
trunkey2003 <21521049@gm.uit.edu.vn>
2023-03-06 10:39:34 +0700

    fix bug

--- app/controllers/google.controller.js
+++ app/controllers/google.controller.js
@@ -1,16 +1,17 @@
 const respond =  require("../services/respond.service");
 const axios = require('axios');
-const fs = require('fs/promises');
+// const fs = require('fs/promises');
 const googleService = require('../services/google.service');
 
 class googleController {
     index(req, res) {
+       try {
         let searchQuery = req.query.searchQuery;
         if (!searchQuery) return respond({res, statusCode: 400, message: "Bad request"});
         const q = searchQuery.replace(/ /g, '+');
         axios.get(`https://www.google.com/search?q=${q}&gl=us&hl=en`)
             .then((reponse) => {
-                fs.writeFile(`${process.cwd()}/output/index.html`, reponse.data)
+                // fs.writeFile(`${process.cwd()}/output/index.html`, reponse.data)
                 const searchGoogleHtml = reponse.data;
                 const result = googleService.getPeopleAlsoAskQuestions({searchQuery, searchGoogleHtml});
                 res.send(result);
@@ -19,6 +20,10 @@ class googleController {
                 console.log(err);
                 respond({res, statusCode: 500, message: "Internal server error"});    
             })
+       }catch {
+        console.log(err);
+        respond({res, statusCode: 500, message: "Internal server error"});
+       }
     }
 };
 
